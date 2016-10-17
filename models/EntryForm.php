b026657e0ef8197c278f3c29a2859f4342fb348b
Yurii Chmil <chmil.yuriy@wetelo.com>
2016-10-17 12:39:29 +0300

    first commit

---
+++ models/EntryForm.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuriy
+ * Date: 10/17/16
+ * Time: 12:34 PM
+ */
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class EntryForm extends Model
+{
+    public $name;
+    public $email;
+
+    public function rules()
+    {
+        return [
+            [['name', 'email'], 'required'],
+            ['email', 'email'],
+        ];
+    }
+}
---
+++ views/site/say.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuriy
+ * Date: 10/17/16
+ * Time: 12:07 PM
+ */
+
+echo $message;
